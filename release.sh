6f57da8eb2cdbbaeaf774afeeea20e1ac69d1c11
viniciusferraria <vinicius.ferraria@conductor.com.br>
2019-07-19 21:04:48 -0300

    added patch,minor,major upgrade

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 access_token=efef76433f27e09568a91e58deaf025f295e45aa
-tag_name_patch=1
-tag_name_minor=1
+
+tag_name_patch=3
+tag_name_minor=2
 tag_name_major=1
 
 echo -n "Do you want to type the tag name? (y/n)"
@@ -13,20 +13,20 @@ do
     read answer
 done
 if [[ $answer == "y" ]]; then
-    echo -n "Patch: "
-    read tag_name_patch
-    echo -n "Minor: "
-    read tag_name_minor
     echo -n "Major: "
     read tag_name_major
+    echo -n "Minor: "
+    read tag_name_minor
+    echo -n "Patch: "
+    read tag_name_patch
     sed -i "4s/.*tag.*/tag_name_patch=$tag_name_patch/" release.sh
     sed -i "5s/.*tag.*/tag_name_minor=$tag_name_minor/" release.sh
     sed -i "6s/.*tag.*/tag_name_major=$tag_name_major/" release.sh
 else
      echo -e "Version upgrade?
-p (Patch == + 0.0.1)
-m (Minor == + 0.1.0)
-mj (Major == + 1.0.0)"
+p (Patch == + x.x.1)
+m (Minor == + x.1.x)
+mj (Major == + 1.x.x)"
     read tag_increment
     while [ "$tag_increment" != "p" ] && [ "$tag_increment" != "m" ] && [ "$tag_increment" != "mj" ]
     do
@@ -37,90 +37,90 @@ mj (Major == + 1.0.0)"
         read tag_increment
     done 
     if [[ $tag_increment == 'p' ]];then
-        tag_name_patch = $((tag_name_patch + 1))
+        tag_name_patch=$(($tag_name_patch + 1))
         sed -i "4s/.*tag.*/tag_name_patch=$tag_name_patch/" release.sh
     elif [[ $tag_increment == 'm' ]];then
-        tag_name_major = $((tag_name_minor + 1))
+        tag_name_major=$(($tag_name_minor + 1))
         sed -i "5s/.*tag.*/tag_name_minor=$tag_name_minor/" release.sh
     else
-        tag_name_major = $((tag_name_major + 1))
+        tag_name_major=$(($tag_name_major + 1))
         sed -i "6s/.*tag.*/tag_name_major=$tag_name_major/" release.sh
 
     fi
 fi
 
 tag_name=""$tag_name_major"."$tag_name_minor"."$tag_name_patch""
-echo $tag_name
-# echo -n "Target commitish: "
-# read target_commitish
+
+echo -n "Target commitish: "
+read target_commitish
 
 
-# echo -n "Name: "
-# read name
+echo -n "Name: "
+read name
 
 
-# echo -n "Body? (y/n) "
-# read body
-# while [ "$body" != "y" ] && [ "$body" != "n" ]
-# do
-#     echo -n "Body? (y/n) "
-#     read body
-# done
-# if [[ $body == 'y' ]]; then
-#     echo -n "Body: "
-#     read body
-# else
-#     body="Release of $tag_name"
-# fi
+echo -n "Body? (y/n) "
+read body
+while [ "$body" != "y" ] && [ "$body" != "n" ]
+do
+    echo -n "Body? (y/n) "
+    read body
+done
+if [[ $body == 'y' ]]; then
+    echo -n "Body: "
+    read body
+else
+    body="Release of $tag_name"
+fi
 
 
-# echo -n "Draft? (y/n) "
-# read draft
-# while [ "$draft" != "y" ] && [ "$draft" != "n" ]
-# do
-#     echo -n "Draft? (y/n) "
-#     read draft
-# done
-# if [[ $draft == "y" ]]; then
-#     draft=true
-# else
-#     draft=false
-# fi
+echo -n "Draft? (y/n) "
+read draft
+while [ "$draft" != "y" ] && [ "$draft" != "n" ]
+do
+    echo -n "Draft? (y/n) "
+    read draft
+done
+if [[ $draft == "y" ]]; then
+    draft=true
+else
+    draft=false
+fi
 
 
-# echo -n "Prerelease? (y/n) "
-# read prerelease
-# while [ "$prerelease" != "y" ] && [ "$prerelease" != "n" ]
-# do
-#     echo -n "Prerelease? (y/n) "
-#     read prerelease
-# done
-# if [[ $prerelease == "y" ]]; then
-#     prerelease=true
-# else
-#     prerelease=false
-# fi
+echo -n "Prerelease? (y/n) "
+read prerelease
+while [ "$prerelease" != "y" ] && [ "$prerelease" != "n" ]
+do
+    echo -n "Prerelease? (y/n) "
+    read prerelease
+done
+if [[ $prerelease == "y" ]]; then
+    prerelease=true
+else
+    prerelease=false
+fi
 
-# if [[ $access_token == "0" ]]; then
-#     echo -n "Access token: "
-#     read new_token
-#     sed -i "3s/.*access_token.*/access_token=$new_token/" release.sh
-#     access_token=$new_token
-# fi 
+if [[ $access_token == "0" ]]; then
+    echo -n "Access token: "
+    read new_token
+    sed -i "2s/.*access_token.*/access_token=$new_token/" release.sh
+    access_token=$new_token
+fi 
 
-# echo '{
-#     "tag_name": "'"${tag_name}"'",
-#     "target_commitish": "'"${target_commitish}"'",
-#     "name": "'"${name}"'",
-#     "body": "'"${body}"'",
-#     "draft": '$draft',
-#     "prerelease": '$prerelease'
-# }'   
+echo '{
+    "tag_name": "'"${tag_name}"'",
+    "target_commitish": "'"${target_commitish}"'",
+    "name": "'"${name}"'",
+    "body": "'"${body}"'",
+    "draft": '$draft',
+    "prerelease": '$prerelease'
+}'   
 
-# echo -n "Do you want to confirm? (y/n): "
-# read confirmation
-# if [[ $confirmation == "y" ]]; then
-#     curl -d '{"tag_name": "'"${tag_name}"'","target_commitish": "'"${target_commitish}"'","name": "'"${name}"'","body": "'"${body}"'","draft": '$draft',"prerelease": '$prerelease'}' https://api.github.com/repos/cdt-baas/m2y_lib_android/releases?access_token=$access_token
-# else
-#     echo -n "Closing..."
-# fi
+echo -n "Do you want to confirm? (y/n): "
+read confirmation
+if [[ $confirmation == "y" ]]; then
+    curl -d '{"tag_name": "'"${tag_name}"'","target_commitish": "'"${target_commitish}"'","name": "'"${name}"'","body": "'"${body}"'","draft": '$draft',"prerelease": '$prerelease'}' https://api.github.com/repos/cdt-baas/m2y_lib_android/releases?access_token=$access_token
+else
+    echo "Closing..."
+fi
